72eb55a78851176487b98a5c1b617d1c52e9dff4
mrcsbda <macbedoya@gmail.com>
2023-07-13 17:10:10 -0500

    endpoints

---
+++ src/services/data.js
@@ -0,0 +1,10 @@
+const URL_API = "https://back-cinema.onrender.com/"
+
+export const endpoints = {
+    urlMovies: `${URL_API}movies`,
+    urlTeatros: `${URL_API}cinemas`,
+    urlCinemaShows: `${URL_API}cinema_shows`,
+    urlTickets: `${URL_API}tickets`,
+    urlHistory: `${URL_API}history`,
+    urlUsers: `${URL_API}users`,
+}
