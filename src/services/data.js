ed21d50bc7c3255e4c09a3fa6f2b54b1568f530c
mrcsbda <macbedoya@gmail.com>
2023-07-14 15:06:16 -0500

    endpoints

--- src/services/data.js
+++ src/services/data.js
@@ -1,10 +1,14 @@
-const URL_API = "https://back-cinema.onrender.com/"
+const URL_API_JSON = "https://back-cinema.onrender.com/"
+export const URL_API_TMDB = "https://api.themoviedb.org/3/movie/now_playing?api_key=1feb292ba6b836164398cc7b3245ab59&language=es-ES"
 
 export const endpoints = {
-    urlMovies: `${URL_API}movies`,
-    urlTeatros: `${URL_API}cinemas`,
-    urlCinemaShows: `${URL_API}cinema_shows`,
-    urlTickets: `${URL_API}tickets`,
-    urlHistory: `${URL_API}history`,
-    urlUsers: `${URL_API}users`,
+    urlTeatros: `${URL_API_JSON}cinemas`,
+    urlCinemaShows: `${URL_API_JSON}cinema_shows`,
+    urlTickets: `${URL_API_JSON}tickets`,
+    urlHistory: `${URL_API_JSON}history`,
+    urlUsers: `${URL_API_JSON}users`,
+}
+
+export const endpointTrailer = (id) => {
+    return `https://api.themoviedb.org/3/movie/${id}/videos?api_key=1feb292ba6b836164398cc7b3245ab59&language=es-ES`;
 }
