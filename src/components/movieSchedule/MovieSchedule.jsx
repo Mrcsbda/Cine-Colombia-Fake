5125159cbd68a40886a70e27d04315e73c2729b3
mrcsbda <macbedoya@gmail.com>
2023-07-21 17:09:10 -0500

    :lipstick: movie schedule styles

--- src/components/movieSchedule/MovieSchedule.jsx
+++ src/components/movieSchedule/MovieSchedule.jsx
@@ -7,7 +7,8 @@ const MovieSchedule = ({ movie, trailer }) => {
       <div className='movie__details'>
         <div className='movie__details__content'>
           <img className='movie__details__poster' src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title} />
-          <h2 className='movie__title'>{movie.title}</h2>
+         <div className='movie__details__container'>
+         <h2 className='movie__title'>{movie.title}</h2>
           <p className='movie__text'>{movie.original_title}</p>
           <div className='movie__details__info-container'>
             <p className='movie__details__classification'>{movie.adult ? "NC-17" : "G"}</p>
@@ -17,20 +18,23 @@ const MovieSchedule = ({ movie, trailer }) => {
                 movie.genres.map(genre => (<span key={genre.id}> {genre.name} </span>))
               }
             </p>
+         </div>
           </div>
         </div>
-        <h3 className='movie__subtitle'>Trailer</h3>
-        <iframe
+        <div>
+          <h3 className='movie__subtitle'>Trailer</h3>
+          <iframe
 
-          className='movie__details__trailer'
-          src={`https://www.youtube.com/embed/${trailer.key}`}
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen></iframe>
-        <h3 className='movie__subtitle'>Sinopsis</h3>
-        <p className='movie__details__overview'>{movie.overview}</p>
+            className='movie__details__trailer'
+            src={`https://www.youtube.com/embed/${trailer.key}`}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen></iframe>
+          <h3 className='movie__subtitle'>Sinopsis</h3>
+          <p className='movie__details__overview'>{movie.overview}</p>
+        </div>
       </div>
-      <hr className='movie__separation-line'/>
+      <hr className='movie__separation-line' />
       <div className='movie__schedule'>
         <h2>Horarios disponibles: 14 Ago 2023</h2>
         <p className='movie__text'>Elige el horario que prefieras</p>
