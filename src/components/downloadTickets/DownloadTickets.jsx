b9e9b17417ac983260c000134ed2609dfb18da69
mrcsbda <macbedoya@gmail.com>
2023-07-25 18:30:38 -0500

    :lipstick: download tickets styles

--- src/components/downloadTickets/DownloadTickets.jsx
+++ src/components/downloadTickets/DownloadTickets.jsx
@@ -1,28 +1,39 @@
 import React from 'react'
+import "./downloadTickets.scss"
 
 const DownloadTickets = ({ props }) => {
-  return (
-    <div>
-        <div>
-            <div>
-                <h2></h2>
-                <div>
-                    <p></p>
-                    <p></p>
+    return (
+        <div className='download-tickets'>
+            <div className='download-tickets__container'>
+                <div className='download-tickets__title-container'>
+                    <h2 className='download-tickets__title'>Boletos</h2>
+                    <div className='download-tickets__date-container'>
+                        <p className='download-tickets__date'>14 Ago</p>
+                        <p className='download-tickets__hour'>7:30 PM</p>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <img src="" alt="" />
-                <div>
-                    <p></p>
-                    <p></p>
-                    <p></p>
-                    <p></p>
+                <div className='download-tickets__info-container'>
+                    <img
+                        className='download-tickets__poster'
+                        src={`https://image.tmdb.org/t/p/original${props.movie.poster_path}`}
+                        alt={props.movie.title} />
+                    <div className='download-tickets__content-container'>
+                        <p className='download-tickets__text'><strong>Pelicula:</strong> {props.movie.title}</p>
+                        <p className='download-tickets__text'><strong>Complejo:</strong> Los Molinos</p>
+                        <p className='download-tickets__text'><strong>Asientos:</strong></p>
+                        <p className='download-tickets__text'><strong>Número de sala:</strong></p>
+                        <figure className='download-tickets__qr-container'>
+                            <img
+                                className='download-tickets__qr'
+                                src="https://pngimg.com/uploads/qr_code/qr_code_PNG25.png"
+                                alt="código qr" />
+                        </figure>
+                    </div>
                 </div>
+
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default DownloadTickets
