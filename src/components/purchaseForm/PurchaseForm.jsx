562bf624845143db33aa1dde3ea2664a693566c8
mrcsbda <macbedoya@gmail.com>
2023-07-25 11:05:51 -0500

    :construction: working in purchase form component

--- src/components/purchaseForm/PurchaseForm.jsx
+++ src/components/purchaseForm/PurchaseForm.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const PurchaseForm = () => {
   return (
-    <div>PurchaseForm</div>
+    <div>
+      <h2>Información Personal</h2>
+      <p>Completa los datos del formulario para realizar el pago</p>
+      <form>
+        <label>
+          Correo Electronico
+          <input type="email" />
+        </label>
+      </form>
+    </div>
   )
 }
 
